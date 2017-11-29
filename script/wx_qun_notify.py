2941e41b792515c1eb1155142cea07f9fddfd180
bingoo <bingoo.huang@gmail.com>
2017-11-29 10:09:07 +0800

    update wx bot script

--- script/wx_qun_notify.py
+++ script/wx_qun_notify.py
@@ -3,20 +3,45 @@
 
 import wxpy
 import time
+import os
 
+from optparse import OptionParser
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-DIRECTORY_TO_WATCH = "./msgs"
+user_home = os.path.expanduser("~")
+
+parser = OptionParser()
+parser.add_option("-p", "--path", dest="msg_path", default=user_home + '/.qun_msg/',
+                  help="read message files from the path", metavar="Path")
+parser.add_option("-q", "--qun", dest="qun_name", default='奕起嗨运行保障群',
+                  help="wx qun name", metavar="QunName")
+
+(options, args) = parser.parse_args()
+
+print('qun_name:' + options.qun_name)
+print('msg_path:' + options.msg_path)
+
+# 初始化机器人，扫码登陆
+bot = wxpy.Bot(console_qr=True, cache_path=True)
+qun = wxpy.ensure_one(bot.groups().search(options.qun_name.decode('utf-8')))
+
+
+def send_msg(src_path):
+    with open(src_path, 'r') as msg_file:
+        msg = msg_file.read()
+        qun.send(msg.decode('utf-8'))
 
 
 class Watcher:
     def __init__(self):
         self.observer = Observer()
+        if not os.path.exists(options.msg_path):
+            os.makedirs(options.msg_path)
 
     def run(self):
         event_handler = Handler()
-        self.observer.schedule(event_handler, DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.schedule(event_handler, options.msg_path, recursive=True)
         self.observer.start()
         try:
             while True:
@@ -28,29 +53,22 @@ class Watcher:
         self.observer.join()
 
 
-# 初始化机器人，扫码登陆
-bot = wxpy.Bot(console_qr=True, cache_path=True)
-qun = wxpy.ensure_one(bot.groups().search(u'奕起嗨运行保障群'))
-
-
-def send_msg(src_path):
-    with open(src_path, 'r') as msg_file:
-        msg = msg_file.read()
-        qun.send(msg.decode('utf-8'))
-
-
 class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
+        event_src_path = event.src_path
         if event.is_directory:
             return None
 
         elif event.event_type == 'created':
-            send_msg(event.src_path)
+            send_msg(event_src_path)
+            os.remove(event_src_path)
 
         elif event.event_type == 'modified':
-            send_msg(event.src_path)
+            send_msg(event_src_path)
+            os.remove(event_src_path)
 
 
-w = Watcher()
-w.run()
+watcher = Watcher()
+watcher.run()
+bot.join()
