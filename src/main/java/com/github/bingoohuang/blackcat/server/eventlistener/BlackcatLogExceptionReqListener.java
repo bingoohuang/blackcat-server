9c21c6e55276c3458e6ebc40843ebe54369ced7e
bingoo <bingoo.huang@gmail.com>
2018-05-30 16:15:10 +0800

    LogId link

--- src/main/java/com/github/bingoohuang/blackcat/server/eventlistener/BlackcatLogExceptionReqListener.java
+++ src/main/java/com/github/bingoohuang/blackcat/server/eventlistener/BlackcatLogExceptionReqListener.java
@@ -16,13 +16,14 @@ public class BlackcatLogExceptionReqListener implements BlackcatReqListener {
 
     @Subscribe
     public void deal(BlackcatLogExceptionReq req) {
+        String logId = req.getLogId();
         val msg = new StringBuilder()
-                .append("Host:").append(req.getHostname())
-                .append("\nTs:").append(req.getTimestamp())
-                .append("\nLogger:").append(req.getLogger())
-                .append("\nTcode:").append(req.getTcode())
-                .append("\nLogId:").append("<a href=\"http://hbm.easy-hi.cn/blackcat-detail/log/" + req.getLogId() + "\">" + req.getLogId() + "</a>")
-                .append("\nEx:").append(req.getExceptionNames());
+                .append("Host: ").append(req.getHostname())
+                .append("\nTs: ").append(req.getTimestamp())
+                .append("\nLogger: ").append(req.getLogger())
+                .append("\nTcode: ").append(req.getTcode())
+                .append("\n<a href=\"http://hbm.easy-hi.cn/blackcat-detail/log/" + logId + "\">LogId</a>: " + logId)
+                .append("\nEx: ").append(req.getExceptionNames());
 
         msgService.sendMsg("日志异常", msg.toString());
         eventDao.addLogException(req);
